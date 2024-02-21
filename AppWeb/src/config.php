fb9948fc658ff4de16cbdc685a26e764049dfccd
sergiii14 <145983411+sergiii14@users.noreply.github.com>
2024-02-21 10:24:28 +0100

    Update config.php

--- AppWeb/src/config.php
+++ AppWeb/src/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '34.234.78.216');
 define('DB_NAME', 'lamp_db');
-define('DB_USER', 'lamp_user');
-define('DB_PASSWORD', 'lamp_password');
+define('DB_USER', 'MiAplicacionUser');
+define('DB_PASSWORD', 'MiAplicacionPass');
 
 $mysqli = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
