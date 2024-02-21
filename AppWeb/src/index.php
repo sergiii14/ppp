b0c4195eab6ae76ca49419c2b86033afb9800549
sergiii14 <145983411+sergiii14@users.noreply.github.com>
2024-02-21 10:15:54 +0100

    Add files via upload

---
+++ AppWeb/src/index.php
@@ -0,0 +1,53 @@
+<?php
+// including the database connection file
+include_once("config.php");
+
+// fetching data in descending order (lastest entry first)
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Homepage</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"  crossorigin="anonymous">	
+</head>
+
+<body>
+<div class = "container">
+	<div class="jumbotron">
+			<h1 class="display-4">Directorio Usuarios</h1>
+			<p class="lead">Examen Ordinaria 23-24</p>
+    </div>	
+	<a href="add.html" class="btn btn-primary">Add New Data</a><br/><br/>
+	<table width='80%' border=0 class="table">
+
+	<tr bgcolor='#CCCCCC'>
+		<td>Nombre</td>
+		<td>Edad</td>
+		<td>Email</td>
+		<td>Update</td>
+	</tr>
+
+	<?php
+	while($res = mysqli_fetch_array($result)) {
+		echo "<tr>\n";
+		echo "<td>".$res['name']."</td>\n";
+		echo "<td>".$res['age']."</td>\n";
+		echo "<td>".$res['email']."</td>\n";
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>\n";
+		echo "</tr>\n";
+	}
+
+	mysqli_close($mysqli);
+	?>
+	</table>
+
+
+	<h3> Examen Ordinaria 23-24 </h3>
+</div>
+
+	
+</body>
+</html>
